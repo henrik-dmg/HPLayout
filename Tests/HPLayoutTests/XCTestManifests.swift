259a1393f9db338697b3993f398a7f2a31ad253f
Henrik Panhans <henrik@panhans.dev>
2020-02-06 10:37:28 +0100

    Updated LayoutProxy

--- Tests/HPLayoutTests/XCTestManifests.swift
+++ Tests/HPLayoutTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(HPLayoutTests.allTests),
+        testCase(HPLayoutTests.allTests)
     ]
 }
 #endif
